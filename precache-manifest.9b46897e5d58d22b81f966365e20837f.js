d6410ab4924ddcb574398af22d9c2eca40548d33
elmehdirahimi <elmehdirahimi@gmail.com>
2021-06-14 11:33:18 +0100

    first commit

---
+++ precache-manifest.9b46897e5d58d22b81f966365e20837f.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1796b9aac0088bcb2bf2935ae701ccd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "143d5ef144017ab5aeaf",
+    "url": "/static/css/3.6a352120.chunk.css"
+  },
+  {
+    "revision": "2d6d9dd7c43574853f0b",
+    "url": "/static/css/4.6e14a8dd.chunk.css"
+  },
+  {
+    "revision": "cf4795ba8da0bef466cf",
+    "url": "/static/css/main.bbfe70e5.chunk.css"
+  },
+  {
+    "revision": "4417fe4aa6d263f2a554",
+    "url": "/static/js/2.461b5d72.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.461b5d72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "143d5ef144017ab5aeaf",
+    "url": "/static/js/3.07446aa1.chunk.js"
+  },
+  {
+    "revision": "2d6d9dd7c43574853f0b",
+    "url": "/static/js/4.a2d1bbb1.chunk.js"
+  },
+  {
+    "revision": "cf4795ba8da0bef466cf",
+    "url": "/static/js/main.c76ac0d3.chunk.js"
+  },
+  {
+    "revision": "5741fedcc192bb3492a4",
+    "url": "/static/js/runtime-main.42ee7910.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "da9b363ebbab586a50b5b0e0ffd0b2b7",
+    "url": "/static/media/aws.da9b363e.svg"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "2db97d69ba44f8da4663584e8197143f",
+    "url": "/static/media/css.2db97d69.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "0567bb2787d0f50492238b5b98008370",
+    "url": "/static/media/docker.0567bb27.svg"
+  },
+  {
+    "revision": "1ae738f7e4864da3f39e4f17687baf02",
+    "url": "/static/media/ensa.1ae738f7.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "4de36d22ea8c41bbb6a8034659a2a7f8",
+    "url": "/static/media/html.4de36d22.svg"
+  },
+  {
+    "revision": "00d7cdd34f1b8658a448759901a55748",
+    "url": "/static/media/java.00d7cdd3.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "9dfa0ae0af580979f374c33ad3c9d88c",
+    "url": "/static/media/nodejs.9dfa0ae0.svg"
+  },
+  {
+    "revision": "21107776d71d006c5199ab373961a674",
+    "url": "/static/media/npm.21107776.svg"
+  },
+  {
+    "revision": "37814477e90eb8012910050e8308ad1f",
+    "url": "/static/media/ocp.37814477.jpg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "8c97ffb99cd1e312745ae3034bb89ca6",
+    "url": "/static/media/python.8c97ffb9.svg"
+  },
+  {
+    "revision": "c74891be3341c4b1ec447e02b9f4c7f1",
+    "url": "/static/media/react.c74891be.svg"
+  },
+  {
+    "revision": "9bfd9f08f28c5c185eb89aa98a74088d",
+    "url": "/static/media/reactNative.9bfd9f08.svg"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "449e8efbf3e642eecc4d8e0b1fe7cb1d",
+    "url": "/static/media/springBoot.449e8efb.svg"
+  },
+  {
+    "revision": "c57ae131ef5e2d371f5a8502d6d6a19a",
+    "url": "/static/media/sql.c57ae131.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
